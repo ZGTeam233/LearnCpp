59d57251b56383e5a20a362516b2e95bc98630ba
ZGTeam233 <wsnzg6@outlook.com>
2026-07-09 13:45:13 +0800

    -

--- cpp26test/main.cpp
+++ cpp26test/main.cpp
@@ -7,8 +7,31 @@ void hello() {
     cout << "Hello from thread " << this_thread::get_id() << endl;
 }
 
+void _print(int a, const string &s) {
+    cout << a << ": " << s << endl;
+}
+
+void test1() {
+    std::thread t(hello); // 线程 t 开始执行 hello()
+    t.join(); // 等待线程结束
+}
+
+void test2() {
+    string str = "hello";
+    thread t1(_print, 42, str);
+    thread t2(_print, 42, cref(str));
+    t1.join();
+    t2.join();
+}
+
 int main() {
-    thread t(hello);
-    t.join();
+    int choice = 0;
+    cout << ": ";
+    cin >> choice;
+    switch (choice) {
+        case 1: test1(); break;
+        case 2: test2(); break;
+        default: break;
+    }
     return 0;
 }
